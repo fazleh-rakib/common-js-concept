410c5f6d27d5fa76858d20b3c736cbbd450b040b
fazleh-rakib <rakibfazleh@gmail.com>
2023-11-28 20:17:45 +0600

    callback function

---
+++ callBack.js
@@ -0,0 +1,9 @@
+function getting (getPera , name){ 
+    getPera(name)
+}
+
+function getPera (name){
+    console.log('Munni Tore Khiya dibo ', name);
+}
+getting(getPera, 'maruf')
+getting(getPera, 'Mizan')
