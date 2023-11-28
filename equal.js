876aa413ac99621adf07ebe68ae5b48f3aa97dab
fazleh-rakib <rakibfazleh@gmail.com>
2023-11-28 16:51:12 +0600

    first commit

---
+++ equal.js
@@ -0,0 +1,14 @@
+const a = 5
+const b= '5'
+if (a==b) {
+    console.log('value is equal');
+} else {
+    console.log('not equal');
+}
+
+if(a===b){
+    console.log('equal');
+}
+else{
+    console.log('not equal');
+}
---
+++ types.js
@@ -0,0 +1,10 @@
+let a = 5
+let b = a
+console.log(a,b);
+ b =7
+console.log(a,b);
+
+let x = {job: 'web developer'}
+let y =x
+y.job ='backend ddd'
+console.log(x,y);
